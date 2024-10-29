d3adcbe02380a2deaff3a361ad9eb597d27c1440
Dani-dzoe <danieldzotepe568@gmail.com>
2024-10-29 18:44:05 +0000

    auth for users feature added

--- source/__init__.py
+++ source/__init__.py
@@ -1,16 +1,46 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+
+db = SQLAlchemy()
+DB_NAME = 'database.sqlite3'
+
+
+def create_database():
+    db.create_all()
+    print('Database Created')
 
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'QU1CK B0Y 111 0A'
-    
-    from .views import view_b
-    from .auth import auth_b
-    from .admin import admin_b
+    app.config['SECRET_KEY'] = 'hbnwdvbn ajnbsjn ahe'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
 
-    app.register_blueprint(view_b, url_prefix='/')
-    app.register_blueprint(auth_b, url_prefix='/')
-    app.register_blueprint(admin_b, url_prefix='/')
+    db.init_app(app)
+
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template('404.html')
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'auth.login'
+
+    @login_manager.user_loader
+    def load_user(id):
+        return Customer.query.get(int(id))
+
+    from .views import views
+    from .auth import auth
+    from .admin import admin
+    from .models import Customer, Cart, Product, Order
+
+    app.register_blueprint(views, url_prefix='/') # localhost:5000/about-us
+    app.register_blueprint(auth, url_prefix='/') # localhost:5000/auth/change-password
+    app.register_blueprint(admin, url_prefix='/')
+
+    with app.app_context():
+        create_database()
 
     return app
